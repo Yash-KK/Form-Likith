913ce47c8fe2dd2229fac71659b1ccbb81179884
Yash-KK <reachyashk@gmail.com>
2025-03-10 13:28:14 +0530

    acno as search field

--- back-end/index.js
+++ back-end/index.js
@@ -4,18 +4,18 @@ import { prisma } from "./lib/prisma.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
 app.get("/health", (req, res) => {
-    res.json({
-      status: true,
-      message: "all good"
-    })  
-})
+  res.json({
+    status: true,
+    message: "all good",
+  });
+});
 
 app.post("/submit-form", async (req, res) => {
   try {
-    const {formData} = req.body;
+    const { formData } = req.body;
     await prisma.voterDetails.create({
       data: { ...formData },
     });
@@ -24,6 +24,24 @@ app.post("/submit-form", async (req, res) => {
   } catch (error) {
     console.error("Error submitting form:", error);
     res.status(500).json({ success: false, message: "Submission failed." });
+  }
+});
+
+app.get("/find-form", async (req, res) => {
+  const { acno } = req.query;
+  try {
+    const formData = await prisma.voterDetails.findMany({
+      where: { acno: acno },
+    });
+
+    if (formData.length > 0) {
+      res.json({ success: true, formData });
+    } else {
+      res.json({ success: false, message: "No data found." });
+    }
+  } catch (error) {
+    console.error("Error fetching form data:", error);
+    res.status(500).json({ success: false, message: "Fetch failed." });
   }
 });
 
